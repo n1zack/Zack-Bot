2b78900b4e4de6f6d8d2eb25d411562d2625edc6
n1zack <zack03ali@gmail.com>
2026-08-22 12:20:19 +0300

    Delete handlers/handlers/handlers/callbacks.py

--- handlers/handlers/handlers/callbacks.py
+++
@@ -1,7 +0,0 @@
-from database import get_user_phones, delete_phone_by_id
-from keyboards import main_keyboard, admin_keyboard, my_numbers_keyboard
-import config
-
-async def handle_callback_query(client, callback_query):
-    # هذه دالة مساعدة للمعالجة
-    pass
